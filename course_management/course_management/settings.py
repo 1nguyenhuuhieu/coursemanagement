f83b66894a6a125e527ff5f8fa203dab089fd228
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-11-09 15:30:22 +0700

    f

--- course_management/course_management/settings.py
+++ course_management/course_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CM',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi-VN'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 USE_I18N = True
 
