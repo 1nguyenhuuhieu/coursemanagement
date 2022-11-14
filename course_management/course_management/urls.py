4ce2accd1ec13677c1b4136e7a10aaf54a2f266e
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-11-14 15:36:03 +0700

    d

--- course_management/course_management/urls.py
+++ course_management/course_management/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Trang quản lý bán khóa học tiếng anh"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('CM.urls')),
